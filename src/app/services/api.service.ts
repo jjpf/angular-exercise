3eac94b7052755c42198904fd6111e5b2c760c8a
Julio José Piñar Figueroa <juliojpf@Julios-MBP.home>
2022-10-14 12:23:43 +0200

    Add a new function to retrieve all acounts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
   // TODO Nice to be a generic class, so we don't need to use 'any' as a type
   protected mock: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
   getList(from: number, to: number): Observable<any> {
     return of(this.mock.slice(from, to));
+  }
+
+  getAll(): Observable<any> {
+    return of(this.mock);
   }
 }
