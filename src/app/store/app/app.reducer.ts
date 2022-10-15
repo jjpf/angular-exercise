f36ae428c9230e5684c8e7c14f3c36310423dd76
Julio José Piñar Figueroa <juliojpf@Julios-MBP.home>
2022-10-15 14:02:04 +0200

    Some imporvements, bit stuck on the use of the selectors

--- src/app/store/app/app.reducer.ts
+++ src/app/store/app/app.reducer.ts
@@ -1,13 +1,11 @@
-import {createReducer, on} from "@ngrx/store";
-import {createEntityAdapter, EntityAdapter, EntityState} from "@ngrx/entity";
-import {IApp} from "../../model/apps.model";
+import { createReducer, on } from '@ngrx/store';
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+import { IApp } from '../../model/apps.model';
 import * as AppActions from './app.actions';
 
 export const appsFeatureKey = 'apps';
 
-export interface State extends EntityState<IApp> {
-
-}
+export interface State extends EntityState<IApp> {}
 
 export const adapter: EntityAdapter<IApp> = createEntityAdapter<IApp>();
 
@@ -17,9 +15,7 @@ export const reducer = createReducer(
   initialState,
   on(AppActions.loadAppListSuccess, (state, action) => {
     return adapter.setAll(action.results, state);
-  }),
+  })
 );
 
-export const {
-  selectAll,
-} = adapter.getSelectors();
+export const { selectAll } = adapter.getSelectors();
--- src/app/store/user/user.effects.ts
+++ src/app/store/user/user.effects.ts
@@ -1,31 +1,61 @@
 import { Injectable } from '@angular/core';
-import {catchError, map, mergeMap, Observable, of} from "rxjs";
-import {Actions, createEffect, ofType} from "@ngrx/effects";
-import * as UserActions from "./user.actions";
-import {IUser} from "../../model/users.model";
-import {UsersService} from "../../services/users.service";
+import { catchError, map, mergeMap, Observable, of } from 'rxjs';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import * as UserActions from './user.actions';
+import { IUser } from '../../model/users.model';
+import { UsersService } from '../../services/users.service';
+import { AccountsService, AppsService } from 'src/app/services';
+import { IApp } from 'src/app/model/apps.model';
+import { IAccount } from 'src/app/model/accounts.model';
+import { selectAppsState } from '../app/app.selectors';
+import { Store } from '@ngrx/store';
 
 @Injectable()
 export class UserEffects {
-  loadUserList$: Observable<{ results: IUser[] } | { error: any }> = createEffect(() => {
-    return this.actions$.pipe(
-      ofType('[Users Page] Load User List'),
-      mergeMap(() => {
-        // TODO Page block should be dynamic
-        return this.usersService.getList(0, 50).pipe(
-          // TODO Join some data from other collections (each user apps)
-          map((response: any) => {
-            return UserActions.loadUserListSuccess({ results: response });
-          }),
-          catchError((error) => {
-            return of(UserActions.loadUserListFailure({ error }));
-          })
-        );
-      })
-    );
-  });
+  loadUserList$: Observable<{ results: IUser[] } | { error: any }> =
+    createEffect(() => {
+      return this.actions$.pipe(
+        ofType('[Users Page] Load User List'),
+        mergeMap(() => {
+          // TODO Page block should be dynamic
+          return this.usersService.getList(0, 50).pipe(
+            // TODO Join some data from other collections (each user apps)
+            map((users: any) => {
+              // Retrieve all accounts
+              let currentAccounts: IAccount[];
+              const allAccounts = this.accountsService.getAll();
+              allAccounts.subscribe(
+                (accounts: IAccount[]) => (currentAccounts = accounts)
+              );
+              // Better to use selectors?
+              const appsState: any = selectAppsState(this.store);
+              console.log(appsState);
+              // Set apps
+              users.forEach((user: IUser) => {
+                user.apps = currentAccounts.flatMap((account: IAccount) => {
+                  const userApps: IApp[] = [];
+                  if (account.user_id == user.id) {
+                    userApps.push(account.app);
+                  }
+                  return userApps;
+                });
+              });
+
+              console.log(users);
+              return UserActions.loadUserListSuccess({ results: users });
+            }),
+            catchError((error) => {
+              return of(UserActions.loadUserListFailure({ error }));
+            })
+          );
+        })
+      );
+    });
 
   constructor(
     private actions$: Actions,
-    private usersService: UsersService) {}
+    private usersService: UsersService,
+    private accountsService: AccountsService,
+    private store: Store
+  ) {}
 }
